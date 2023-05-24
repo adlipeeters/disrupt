fd157b9a78b06218a2fb0af71f79d85e55aae3cd
adlipeeters <104755659+AndreiUsatii@users.noreply.github.com>
2023-05-24 20:18:01 +0300

    Third commit

--- src/pages/ComingSoon/components/BackgroundVideo.jsx
+++ src/pages/ComingSoon/components/BackgroundVideo.jsx
@@ -3,9 +3,16 @@ import styles from "../style.module.css";
 import images from "../../../constants";
 
 const BackgroundVideo = () => {
+  const videoRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (videoRef) {
+      videoRef.current.play();
+    }
+  }, [videoRef]);
   return (
     <div className={styles.background_video}>
-      <video autoPlay={true} loop muted>
+      <video autoPlay={true} loop muted ref={videoRef}>
         <source src={images.background_video} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
