153bb734692412698075d456945cf6ef1a76c376
adlipeeters <104755659+AndreiUsatii@users.noreply.github.com>
2023-05-24 21:57:11 +0300

    Background Video.8

--- src/pages/ComingSoon/components/BackgroundVideo.jsx
+++ src/pages/ComingSoon/components/BackgroundVideo.jsx
@@ -78,11 +78,22 @@ export default function App() {
     <img src={mainVideo} alt="Muted Video" />
   ) : (
     <div
-      className={styles.background_video}
+      // className={styles.background_video}
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        zIndex: -2,
+      }}
       ref={videoParentRef}
       dangerouslySetInnerHTML={{
         __html: `
         <video
+        style=" width: 100%;
+    height: 100%;
+    object-fit: cover;"
           loop
           muted
           autoplay
