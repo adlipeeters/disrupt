d32125e687a2b83238018e52ef8728fe759cf939
adlipeeters <104755659+AndreiUsatii@users.noreply.github.com>
2023-05-24 20:05:25 +0300

    Second commit

--- src/pages/ComingSoon/components/BackgroundVideo.tsx
+++ src/pages/ComingSoon/components/BackgroundVideo.tsx
@@ -5,7 +5,7 @@ import images from "../../../constants";
 const BackgroundVideo = () => {
   return (
     <div className={styles.background_video}>
-      <video autoPlay loop muted>
+      <video autoPlay={true} loop muted>
         <source src={images.background_video} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
